38f596770dd19d147ddaaadff810d31d4d6c71da
raspgary <garywqc@utexas.edu>
2020-04-25 15:03:28 -0500

    line animation

--- scripts/dashboardscript.js
+++ scripts/dashboardscript.js
@@ -202,37 +202,25 @@ $(document).ready(function () {
 
         var data = google.visualization.arrayToDataTable([
           ['Day', 'Shower', 'Toilet', 'Light'],
-          ['Monday', 1000, 400, 200],
+          ['Monday', 1000, 400, 200], //0,012
           ['Tuesday', 1170, 460, 250],
           ['Wednesday', 660, 1120, 300],
           ['Thursday', 1030, 540, 350],
           ['Friday', 1030, 540, 350],
           ['Saturday', 1030, 540, 350],
-          ['Sunday', 1030, 540, 350],
+          ['Sunday', 1030, 540, 350], //6,012
         ]);
 
-      // var data = new google.visualization.DataTable();
-      // data.addColumn('word', 'Day');
-      // data.addColumn('number', 'Shower');
-      // data.addColumn('number', 'Lights');
-      // data.addColumn('number', 'Toilet');
-
-      // data.addRows([
-      //   ['Monday',  37.8, 80.8, 41.8],
-      //   ['Monday',  30.9, 69.5, 32.4],
-      //   ['Monday',  25.4,   57, 25.7]
-        // [4,  11.7, 18.8, 10.5],
-        // [5,  11.9, 17.6, 10.4],
-        // [6,   8.8, 13.6,  7.7],
-        // [7,   7.6, 12.3,  9.6],
-        // [8,  12.3, 29.2, 10.6],
-        // [9,  16.9, 42.9, 14.8],
-        // [10, 12.8, 30.9, 11.6],
-        // [11,  5.3,  7.9,  4.7],
-        // [12,  6.6,  8.4,  5.2],
-        // [13,  4.8,  6.3,  3.6],
-        // [14,  4.2,  6.2,  3.4]
-      // ]);
+        var dumbieData = google.visualization.arrayToDataTable([
+          ['Day', 'Shower', 'Toilet', 'Light'],
+          ['Monday', 1000, 400, 200],
+          ['Tuesday', 0, 0, 0],
+          ['Wednesday', 0, 0, 0],
+          ['Thursday', 0, 0, 0],
+          ['Friday', 0, 0, 0],
+          ['Saturday', 0, 0, 0],
+          ['Sunday', 0, 0, 0],
+          ]);
 
       var options = {
         chart: {
@@ -250,7 +238,31 @@ $(document).ready(function () {
 
       var chart = new google.charts.Line(document.getElementById('linechart_material'));
 
-      chart.draw(data, google.charts.Line.convertOptions(options));
+     
+      var index = 1;
+      setInterval(function(){
+       
+        if(dumbieData.getValue(index,1) < data.getValue(index, 1)){
+          dumbieData.setValue(index,1, dumbieData.getValue(index,1)+(data.getValue(index,1)/6));
+        }
+        if(dumbieData.getValue(index,2) < data.getValue(index, 2)){
+          dumbieData.setValue(index,2, dumbieData.getValue(index,2)+(data.getValue(index,2)/6));
+        }
+        if(dumbieData.getValue(index,3) < data.getValue(index, 3)){
+          dumbieData.setValue(index,3, dumbieData.getValue(index,3)+(data.getValue(index,3)/6));
+        }
+        if(dumbieData.getValue(index,1)>=data.getValue(index,1) && dumbieData.getValue(index,2)>=data.getValue(index,2) && dumbieData.getValue(index,3)>=data.getValue(index,3))
+        {
+          index++;
+        }
+        // console.log(data.getValue(6,1));
+        // console.log(data.getValue(6,2));
+        // console.log(data.getValue(6,3));
+
+
+        chart.draw(dumbieData, google.charts.Line.convertOptions(options));
+
+      }, 30);
     }
 
 });
